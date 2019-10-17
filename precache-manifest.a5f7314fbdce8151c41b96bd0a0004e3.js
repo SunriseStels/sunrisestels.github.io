3dce5d52784b66d19ff0c90a5151c4fe81c3c227
Dmitry <dima6200@gmail.com>
2019-10-18 00:20:12 +0500

    build

---
+++ precache-manifest.a5f7314fbdce8151c41b96bd0a0004e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db0ef6c7da371f4ee08a3425f58bc20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b6cf79c1a3aff97bdc3",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "7b6cf79c1a3aff97bdc3",
+    "url": "/static/js/2.2317b53b.chunk.js"
+  },
+  {
+    "revision": "822de9236da55c976e67",
+    "url": "/static/js/main.57b9e81a.chunk.js"
+  },
+  {
+    "revision": "52a97042eadd2dfd8cd2",
+    "url": "/static/js/runtime-main.402b3f14.js"
+  }
+]);
